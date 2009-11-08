02d550e0ad7807c699ba2d1395d4fb08a9b98fdc
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2009-11-08 11:13:26 +0100

    Changed specs to be more descriptive, added README note for running scenarios/specs

--- spec/cyrillizer_spec.rb
+++ spec/cyrillizer_spec.rb
@@ -2,13 +2,13 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe "Convertor" do
 
-  it "changes original string" do
+  it "should change original string when using to_cyr!" do
     string = 'orginalen'
     string.to_cyr!.should == 'оргинален'
     string.should == 'оргинален'
   end
 
-  it "does not changes original string" do
+  it "should not change original string when using to_cyr" do
     string = 'orginalen'
     string.to_cyr.should == 'оргинален'
     string.should == 'orginalen'
