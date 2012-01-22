4663f8f2c5f7296544aa77c505bb79e1e01d9795
Aleksandar Simic <asimic@gmail.com>
2012-01-22 15:11:30 +0000

    slowly making the changes to make this more compatible with Serbian Cyrillic

--- spec/cyrillizer_spec.rb
+++ spec/cyrillizer_spec.rb
@@ -5,27 +5,31 @@ require 'spec_helper'
 describe "Convertor" do
 
   it "should change original string when using to_cyr!" do
-    string = 'orginalen'
-    string.to_cyr!.should == 'оргинален'
-    string.should == 'оргинален'
+    string = 'Milisav'
+    string.to_cyr!.should == 'Милисав'
+    string.should == 'Милисав'
   end
 
   it "should not change original string when using to_cyr" do
-    string = 'orginalen'
-    string.to_cyr.should == 'оргинален'
-    string.should == 'orginalen'
+    string = 'Darinka'
+    string.to_cyr.should == 'Даринка'
+    string.should == 'Darinka'
   end
 
   it "should change original string when using to_lat!" do
-    string = 'оргинален'
-    string.to_lat!.should == 'orginalen'
-    string.should == 'orginalen'
+    string = 'Руменка'
+    string.to_lat!.should == 'Rumenka'
+    string.should == 'Rumenka'
   end
 
   it "should not change original string when using to_lat" do
-    string = 'оргинален'
-    string.to_lat.should == 'orginalen'
-    string.should == 'оргинален'
+    string = 'Изворка'
+    string.to_lat.should == 'Izvorka'
+    string.should == 'Изворка'
+  end
+
+  it "should convert Serbian characters" do
+    "chicha".to_cyr.should == 'чича'
   end
 
 end
