c591a65661cf2dd7a59dabaabdd8e52e9550f849
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2022-04-05 22:30:44 +0200

    Set ruby version and upgrade rspec suite

--- spec/cyrillizer/alphabet_spec.rb
+++ spec/cyrillizer/alphabet_spec.rb
@@ -2,35 +2,35 @@
 
 require 'spec_helper'
 
-describe Cyrillizer do
+RSpec.describe Cyrillizer do
   after :each do
     Cyrillizer.language = nil
   end
 
   describe ".language" do
     it "defaults to macedonian language" do
-      Cyrillizer.language.should == :macedonian
+      expect(Cyrillizer.language).to eq(:macedonian)
     end
   end
 
   describe ".language=" do
     it "can specify existing language" do
       Cyrillizer.language = :russian
-      Cyrillizer.language.should == :russian
+      expect(Cyrillizer.language).to eq(:russian)
     end
   end
 
   describe ".alphabet" do
     it "can load alphabet for a language" do
-      Cyrillizer.alphabet["Џ"].should == "Dzh"
+      expect(Cyrillizer.alphabet['Џ']).to eq('Dzh')
     end
   end
 
   describe ".alphabet=" do
     it "can set alphabet and language with alphabet file" do
-      Cyrillizer.alphabet = File.join(File.dirname(__FILE__), "../../lib/alphabets/russian.yml")
-      Cyrillizer.alphabet['Щ'].should == "Shch"
-      Cyrillizer.language.should == 'russian'
+      Cyrillizer.alphabet = File.join(File.dirname(__FILE__), '../../lib/alphabets/russian.yml')
+      expect(Cyrillizer.alphabet['Щ']).to eq('Shch')
+      expect(Cyrillizer.language).to eq('russian')
     end
   end
 end
